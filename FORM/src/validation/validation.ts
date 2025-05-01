6ce1ca75461cd46ce9c15aba12448ca242e9df21
ahmed ashraf elsayed <ahmedashroof89@gmail.com>
2025-05-01 08:34:15 +0300

    Delete FORM directory

--- FORM/src/validation/validation.ts
+++
@@ -1,8 +0,0 @@
-import { check } from "express-validator";
-export const validateuser = () => {
-    return [
-        check("username").notEmpty().withMessage("Name is required"),
-        check("email").isEmail().withMessage("Email is invalid"),
-        check("password").notEmpty().withMessage("Password is required"),
-    ];
-}
